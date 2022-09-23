ae4021105b2f06223f18d0235a0c84b7bcf08d13
Mariia <m.goncharenkoo@icloud.com>
2022-09-23 12:21:02 -0400

    commit tree 7fc0837a8f3d8fba3b87ffd49112de321da35200

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'sam';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'dan';
+}
